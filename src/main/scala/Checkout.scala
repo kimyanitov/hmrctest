b9cba229b717dcffcbf747282182da3cca21c980
Konstantin Imyanitov <koi01@bskyb.com>
2016-10-11 13:16:40 +0100

    KI: checkout total done

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -2,7 +2,7 @@
   * Created by kostya on 11/10/2016.
   */
 object Checkout extends PriceList {
-  def total(basket: Basket, offers: List[Offer with PriceList]): Int = ???
+  def total(basket: Basket, offers: List[Offer with PriceList]): Int = price(basket) + discount(basket, offers)
 
   def discount(basket: Basket, offers: List[Offer]): Int = offers.map(_.discount(basket)).sum
 
