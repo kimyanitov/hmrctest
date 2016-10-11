8d10a59efee68f4013a70f7b2ee7d8dd7efb81db
Konstantin Imyanitov <koi01@bskyb.com>
2016-10-11 12:36:25 +0100

    KI: checkout phase 1 done

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -2,5 +2,5 @@
   * Created by kostya on 11/10/2016.
   */
 object Checkout extends PriceList {
-  def price(basket: Basket): Int = ???
+  def price(basket: Basket): Int = basket.getItems.map(getPrice).sum
 }
